2b1a7ca8ff71fbb03c4ed06223e4202d5e536e58
Victor <victor aguilar@victor-PC>
2012-12-13 16:47:11 -0600

    Alta de llamadas: Da sugerencias de nombres y muestra un ficha completa del nombre seleccionado.

---
+++ busca_ficha.php
@@ -0,0 +1,34 @@
+<?php
+header('Content-type: text/html');
+
+require_once 'configuracion.php';
+require_once 'funciones.php';
+
+$db = dameConexion();
+
+$nombre = $db->real_escape_string($_POST['nombre']);
+
+$query = 'select * from fichas where nombre = "'. $nombre . '" limit 1;';
+
+$result = $db->query( $query );
+$columnas = array();
+$ficha = array();
+
+if( $result ){
+	$columnas = $result->fetch_fields();
+	$ficha = $result->fetch_row();	
+}
+
+$len = count($columnas);
+?>
+<table class="center coloreada">
+	<tr>
+		<th colspan="2">Ficha de la Persona Relacionada</th>
+	</tr>
+<?php for($i=0 ; $i< $len; $i++):?>
+	<tr><td><?php echo $columnas[$i]->name?></td><td><?php echo $ficha[$i] ?></td></tr>
+<?php endfor?>
+	<tr><td></td><td><a href="alta_de_ficha.php?id_ficha=<?php echo $ficha[0]?>" title="Actualizar Ficha">Actualizar Ficha</a> 
+</table>
+<!-- Esta campo se envia en el formulario principal de alta_de_llamadas" (html5) -->
+<input type="hidden" value="<?php echo $ficha[0]?>" name="id_ficha" form="alta_de_llamadas">
